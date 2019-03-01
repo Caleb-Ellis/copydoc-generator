cb5f14cf5b7eafe7cddae5f0e37b3f56dd65f2ac
Caleb Ellis <caleb.ellis@canonical.com>
2019-03-01 11:21:12 +0000

    test

---
+++ precache-manifest.02b0928099d3c44056c2af819f7016d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a6e90784810da1e06524",
+    "url": "/static/js/main.a6e90784.chunk.js"
+  },
+  {
+    "revision": "c075436958bc22cd1a61",
+    "url": "/static/js/2.c0754369.chunk.js"
+  },
+  {
+    "revision": "a6e90784810da1e06524",
+    "url": "/static/css/main.2dd672df.chunk.css"
+  },
+  {
+    "revision": "82b19434e49b66d52373ec55c549d548",
+    "url": "/index.html"
+  }
+];
